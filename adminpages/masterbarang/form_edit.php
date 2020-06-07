6c1eb84c3bb64a58c84d8ba8a62c5b13299e8403
nisvi <nisvimaulidyah@gmail.com>
2020-06-08 00:41:56 +0700

    rapi

--- adminpages/masterbarang/form_edit.php
+++ adminpages/masterbarang/form_edit.php
@@ -35,42 +35,35 @@ include "../templates/header.php"; ?>
 	  <form method="post" id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="aksi_edit.php">
 		<input type="hidden" name="id_barang" value="<?php echo $dataBarang['id_barang'];?>">
 	  <div class="form-group">
+      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">ID Barang <span class="required">*</span></label>
+      <div class="col-md-10 col-sm-10 col-xs-12">
+        <input type="text" id="first-name" name="id_barang" value="<?php echo $dataBarang['id_barang'];?>" required="required" class="form-control col-md-7 col-xs-12">
+      </div><br>
 
-    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">ID Barang <span class="required">*</span>
-    </label>
-    <div class="col-md-10 col-sm-10 col-xs-12">
-      <input type="text" id="first-name" name="id_barang" value="<?php echo $dataBarang['id_barang'];?>" required="required" class="form-control col-md-7 col-xs-12">
-    </div><br>
+      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">ID Pembelian <span class="required">*</span></label>
+      <div class="col-md-10 col-sm-10 col-xs-12">
+        <input type="text" id="first-name" name="id_pembelian" value="<?php echo $dataBarang['id_pembelian'];?>" required="required" class="form-control col-md-7 col-xs-12">
+      </div><br>
 
-      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">ID Pembelian <span class="required">*</span>
-    </label>
-    <div class="col-md-10 col-sm-10 col-xs-12">
-      <input type="text" id="first-name" name="id_pembelian" value="<?php echo $dataBarang['id_pembelian'];?>" required="required" class="form-control col-md-7 col-xs-12">
-    </div><br>
+      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Nama Barang <span class="required">*</span></label>
+		  <div class="col-md-10 col-sm-10 col-xs-12">
+		    <input type="text" id="first-name" name="nama_barang" value="<?php echo $dataBarang['nama_barang'];?>" required="required" class="form-control col-md-7 col-xs-12">
+		  </div><br>
 
-		<label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Nama Barang <span class="required">*</span>
-		</label>
-		<div class="col-md-10 col-sm-10 col-xs-12">
-		  <input type="text" id="first-name" name="nama_barang" value="<?php echo $dataBarang['nama_barang'];?>" required="required" class="form-control col-md-7 col-xs-12">
-		</div><br>
+      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Harga Beli <span class="required"></span></label>
+      <div class="col-md-10 col-sm-10 col-xs-12">
+        <input type="text" id="first-name" name="harga_beli" value="<?php echo $dataBarang['harga_beli'];?>" required="required" class="form-control col-md-7 col-xs-12">
+      </div><br>
 
-    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Harga Beli <span class="required"></span>
-    </label>
-    <div class="col-md-10 col-sm-10 col-xs-12">
-      <input type="text" id="first-name" name="harga_beli" value="<?php echo $dataBarang['harga_beli'];?>" required="required" class="form-control col-md-7 col-xs-12">
-    </div><br>
+     <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Harga Jual Satuan <span class="required"></span></label>
+      <div class="col-md-10 col-sm-10 col-xs-12">
+        <input type="text" id="first-name" name="harga_jualsatuan" value="<?php echo $dataBarang['harga_jualsatuan'];?>" required="required" class="form-control col-md-7 col-xs-12">
+      </div><br>
 
-     <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Harga Jual Satuan <span class="required"></span>
-    </label>
-    <div class="col-md-10 col-sm-10 col-xs-12">
-      <input type="text" id="first-name" name="harga_jualsatuan" value="<?php echo $dataBarang['harga_jualsatuan'];?>" required="required" class="form-control col-md-7 col-xs-12">
-    </div><br>
-
-    <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Stok <span class="required"></span>
-    </label>
-    <div class="col-md-10 col-sm-10 col-xs-12">
-      <input type="text" id="first-name" name="stok" value="<?php echo $dataBarang['stok'];?>" required="required" class="form-control col-md-7 col-xs-12">
-    </div>
+      <label class="control-label col-md-2 col-sm-2 col-xs-12" for="first-name">Stok <span class="required"></span></label>
+      <div class="col-md-10 col-sm-10 col-xs-12">
+        <input type="text" id="first-name" name="stok" value="<?php echo $dataBarang['stok'];?>" required="required" class="form-control col-md-7 col-xs-12">
+      </div>
 
     
 	  </div>
@@ -79,7 +72,7 @@ include "../templates/header.php"; ?>
 	  <div class="form-group">
 		<div class="col-md-3 col-sm-3 col-xs-12 col-md-offset-2">
 		  <button type="submit" class="btn btn-primary">Batal</button>
-		  <button type="submit" class="btn btn-success"><i class="fa fa-save"></i>Simpan</button>
+		  <button type="submit" class="btn btn-success"><i class="fa fa-save"></i> Simpan</button>
 		</div>
 	  </div>
 
