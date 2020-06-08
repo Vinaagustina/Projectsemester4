a910411aebffaa417a6acdf9152d10a5e9cac95f
vinaagustina <vinaagustinan@gmail.com>
2020-06-08 15:50:31 +0700

    tampilan sementara home

--- adminpages/home/main.php
+++ adminpages/home/main.php
@@ -15,8 +15,8 @@ include "../templates/header.php"; ?>
  -->
 
 
-            <!-- </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+
+            <!-- <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-clock-o"></i> Total Barang</span>
               <div class="count">50</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i></i> From last Mounth</span>
@@ -26,23 +26,23 @@ include "../templates/header.php"; ?>
               <div class="count green">20</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i></i> From last Mounth</span>
             </div> -->
-            <!-- <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            <!-- <<div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-user"></i> Total Supplier</span>
               <div class="count">15</div>
               <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i></i> From last Mounth</span>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            </div> -->
+            <!-- <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-user"></i> Total Pembelian</span>
               <div class="count">10</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i></i> From last Mounth</span>
-            </div>
-            <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
+            </div> -->
+            <!-- <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
               <span class="count_top"><i class="fa fa-user"></i> Total Penjualan</span>
               <div class="count">10</div>
               <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i></i> From last Mounth</span>
-            </div>
-          </div> -->
-          <!-- /top tiles
+            </div> -->
+          </div>
+          <top tiles>
 
           <div class="row">
             <div class="col-md-12 col-sm-12 col-xs-12">
@@ -54,59 +54,60 @@ include "../templates/header.php"; ?>
 					<h3><small>Data Anggota KSP Setya Karya</small></h3>
                   </div>
                   <div class="col-md-6">
-                    <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                    <!-- <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
                       <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
                       <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-                    </div>
+                    </div> -->
                   </div>
                 </div>
 
                 <div class="col-md-9 col-sm-9 col-xs-12">
                   <div id="placeholder33" style="height: 260px; display: none" class="demo-placeholder"></div>
                   <div style="width: 100%;">
-                    <div id="canvas_dahs" class="demo-placeholder" style="width: 100%; height:270px;"></div>
+                    <!-- <div id="canvas_dahs" class="demo-placeholder" style="width: 100%; height:270px;"></div> -->
                   </div>
                 </div>
+                </div>
                 <div class="col-md-3 col-sm-3 col-xs-12 bg-white">
-                  <div class="x_title">
+                  <!-- <div class="x_title">
                     <h2>Top Campaign Performance</h2>
                     <div class="clearfix"></div>
-                  </div>
+                  </div> -->
 
                   <div class="col-md-12 col-sm-12 col-xs-6">
                     <div>
-                      <p>Facebook Campaign</p>
+                      <!-- <p>Facebook Campaign</p>
                       <div class="">
                         <div class="progress progress_sm" style="width: 76%;">
                           <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="80"></div>
                         </div>
-                      </div>
+                      </div> -->
                     </div>
                     <div>
-                      <p>Twitter Campaign</p>
+                      <!-- <p>Twitter Campaign</p>
                       <div class="">
                         <div class="progress progress_sm" style="width: 76%;">
                           <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="60"></div>
                         </div>
-                      </div>
+                      </div> -->
                     </div>
                   </div>
                   <div class="col-md-12 col-sm-12 col-xs-6">
                     <div>
-                      <p>Conventional Media</p>
+                      <!-- <p>Conventional Media</p>
                       <div class="">
                         <div class="progress progress_sm" style="width: 76%;">
                           <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="40"></div>
                         </div>
-                      </div>
+                      </div> -->
                     </div>
                     <div>
-                      <p>Bill boards</p>
+                      <!-- <p>Bill boards</p>
                       <div class="">
                         <div class="progress progress_sm" style="width: 76%;">
                           <div class="progress-bar bg-green" role="progressbar" data-transitiongoal="50"></div>
                         </div>
-                      </div>
+                      </div> -->
                     </div>
                   </div>
 
@@ -366,5 +367,5 @@ include "../templates/header.php"; ?>
 
           </div>
         </div>
-        <!-- /page content -->
+        </page content>
 <?php include "../templates/footer.php"; ?>
