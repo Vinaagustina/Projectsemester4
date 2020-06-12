80b4cd7b8fd0655e64b49d112a572ec264096b3d
vinaagustina <vinaagustinan@gmail.com>
2020-06-12 18:32:56 +0700

    tambahan
    tambahan

--- cetak_transaksi_langsung.php
+++ cetak_transaksi_langsung.php
@@ -30,10 +30,9 @@ include 'config.php';
     <body>
             <h3 align="center">LAPORAN TRANSAKSI LANGSUNG</h3>
         <div class="page">
-
+          <h4><b>LIVINA STORE</b></h4>
           SUMBERSARI, JEMBER<br> JAWA TIMUR<br>
           <BR>
-
              Tgl Cetak: <?php echo date('d-m-y') ?>
         <div class="kop">
             <!--<img src="../img/kop.png" id="kop"><br>-->
